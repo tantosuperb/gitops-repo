18fd6cc9b5f62edd07a1147ff427b71aecd0a262
정성영/QA2팀/EP <tantosuperb@gmail.com>
2025-04-30 09:16:37 +0900

    Create HealthCheckController.java

---
+++ app/shopping-web/src/main/java/com/example/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.example; // 패키지는 기존 프로젝트 구조에 맞게 조정
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
